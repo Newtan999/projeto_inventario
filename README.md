83907d3cf3513123dcfef43feb5a63b1d75893b1
Newton Filho <jnewton.fff@gmail.com>
2025-11-02 11:41:11 -0300

    Definicao dos models e atualizacao do README

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # projeto_inventario
-Projeto de inventario de computadores
+Projeto de inventario de equipamentos de TI
+
+Equipamento
+
+Cada item de hardware ou software que você quer registrar.
+
+Campos possíveis: nome, tipo, fabricante, modelo, número de série, data de aquisição, status (em uso, em manutenção, inativo).
+
+Departamento / Setor
+
+Para saber onde cada equipamento está alocado.
+
+Campos: nome do departamento, localização, responsável.
+
+Categoria de Equipamento
+
+Ex: “Desktop”, “Notebook”, “Servidor”, “Monitor”, “Software”.
+
+Fornecedor
+
+Campos: nome, telefone, email, endereço.
+
+Manutenção / Histórico
+
+Para registrar manutenções e alterações de status.
+
+Campos: equipamento (FK), data da manutenção, descrição do problema/solução, técnico responsável.
+
+Relacionamentos:
+
+Um equipamento pertence a um departamento → ForeignKey.
+
+Um equipamento pertence a uma categoria → ForeignKey.
+
+Um equipamento pode ter várias manutenções → OneToMany
+
+Um equipamento pode ter um fornecedor → ForeignKey.
--- inventario/models.py
+++ inventario/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+# Setor
+class Setor(models.Model):
+    nome = models.CharField(max_length=100)
+    
+
+    def __str__(self):
+        return self.nome
+
+# Equipamento
+class Equipamento(models.Model):
+    TIPO_EQUIPAMENTO = [
+        ('desktop', 'Desktop'),
+        ('notebook', 'Notebook'),
+        ('monitor', 'Monitor'),
+        ('servidor', 'Servidor'),
+        ('nobreak', 'Nobreak'),
+        ('impressora', 'Impressora'),
+        ('outro', 'Outro'),
+    ]
+
+    descricao = models.CharField(max_length = 100)
+    data_aquisicao = models.DateField()
+    responsavel = models.CharField(max_length = 100, blank=True, null=True)
+    status = models.CharField(max_length = 30, default = 'Em uso')
+    numero_patrimonio = models.CharField(max_length=9, unique = True)
+    setor = models.ForeignKey(Setor, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.descricao
+    
+# Manutenção
+class Manutencao(models.Model):
+    equipamento = models.ForeignKey(Equipamento, on_delete=models.CASCADE, related_name='manutencoes')
+    data = models.DateField()
+    tecnico = models.CharField(max_length = 100)
+
+    def __str__(self) -> str:
+        return f"Manutenção de {self.equipamento.descricao} em {self.data}"
