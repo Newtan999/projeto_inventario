fe8989ee2e20530bcabd902a07c987c7a91a02fe
Newton Filho <jnewton.fff@gmail.com>
2025-11-03 20:49:28 -0300

    View e forms de equipamento

---
+++ inventario/equipamento/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from .models import Equipamento
+
+class EquipamentoForm(forms.ModelForm):
+    class Meta:
+        model = Equipamento
+        fields = '__all__'
+        widgets = {
+            'data': forms.DateInput(attrs={'type': 'datetime-local'}),
+        }
---
+++ inventario/equipamento/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Equipamento
+from .forms import EquipamentoForm
+
+def listar_equipamentos(request):
+    equipamentos = Equipamento.objects.all()
+    return render(request, 'equipamento/listar_equipamentos.html',{'equipamentos': equipamentos})
+
+def cadastrar_equipamento(request):
+    if request.method == 'POST':
+        form = EquipamentoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('equipamento:listar_equipamento')
+    else:
+        form = EquipamentoForm()
