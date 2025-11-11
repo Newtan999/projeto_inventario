a9361341b58a00f2dac6ca608f5d88de52341507
Newton Filho <jnewton.fff@gmail.com>
2025-11-11 16:14:26 -0300

    Ajustes no layout e funcionalidades do Cadastro de Equipamentos

--- inventario/equipamento/models.py
+++ inventario/equipamento/models.py
@@ -4,7 +4,7 @@ from inventario.setor.models import Setor
 # Equipamento
 class Equipamento(models.Model):
     TIPO_EQUIPAMENTO = [
-        ('desktop', 'Desktop'),
+        ('desktop', 'Desktop'), #(nome, texto exibido)
         ('notebook', 'Notebook'),
         ('monitor', 'Monitor'),
         ('servidor', 'Servidor'),
@@ -12,11 +12,18 @@ class Equipamento(models.Model):
         ('impressora', 'Impressora'),
         ('outro', 'Outro'),
     ]
+    
+    STATUS = [
+        ('em_uso', 'Em uso'),
+        ('obsoleto', 'Obsoleto'),
+        ('com_defeito', 'Com defeito'),
+    ]
 
     descricao = models.CharField(max_length = 100)
+    tipo = models.CharField(max_length=20, choices=TIPO_EQUIPAMENTO, default='outro')
     data_aquisicao = models.DateField()
     responsavel = models.CharField(max_length = 100, blank=True, null=True)
-    status = models.CharField(max_length = 30, default = 'Em uso')
+    status = models.CharField(max_length = 30, choices=STATUS, default = 'Em uso')
     numero_patrimonio = models.CharField(max_length=9, unique = True)
     setor = models.ForeignKey(Setor, on_delete=models.PROTECT)
 
