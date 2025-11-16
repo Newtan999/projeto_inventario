e4d1f4fb3db877fc618ccd57adff304c24507ddd
Newton Filho <jnewton.fff@gmail.com>
2025-11-16 18:43:44 -0300

    Classe de exclusão de equipamentos criada e pequenas correções

--- inventario/equipamento/urls.py
+++ inventario/equipamento/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from . import views
+from .views import ExcluirEquipamentoView
 
 app_name = 'equipamento'
 
 urlpatterns = [
     path('', views.listar_equipamentos, name='listar_equipamentos'),
-    path('novo/', views.cadastrar_equipamento, name='cadastrar_equipamento'),
+    path('novo/', views.cadastrar_equipamento, name='form_equipamentos'),
     #path('editar/<int:id>/', views.editar_equipamento, name='editar_equipamento'),
-    path('excluir/<int:id>/', views.excluir_equipamento, name='excluir_equipamento'),
+    path('excluir/<int:id>/', ExcluirEquipamentoView.as_view, name='excluir_equipamento'),
 
 ]
--- inventario/equipamento/views.py
+++ inventario/equipamento/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.urls import reverse
+from django.views.generic import DeleteView
+from django.urls import reverse_lazy
+from django.contrib import messages
 from .models import Equipamento
 from .forms import EquipamentoForm
 
@@ -7,23 +9,50 @@ def listar_equipamentos(request):
     equipamentos = Equipamento.objects.all()
     return render(request, 'equipamento/listar_equipamentos.html', {'equipamentos': equipamentos})
 
+# CADASTRO DE EQUIPAMENTOS
+
 def cadastrar_equipamento(request):
     if request.method == 'POST':
         form = EquipamentoForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('equipamento:listar_equipamentos', {'form': form})
+            return redirect('equipamento:listar_equipamentos') #! Checar singular
     else:
         form = EquipamentoForm()
     return render(request, 'equipamento/form_equipamentos.html', {'form': form})
 
-def excluir_equipamento(request, id):
-    equipamento = get_object_or_404(Equipamento, id=id)
+# EDIÇÃO DE EQUIPAMENTOS
+
+def editar_equipamento(request, equipamento_id):
+    equipamento = get_object_or_404(Equipamento, id = equipamento_id)
+
     if request.method == 'POST':
-        equipamento.delete()
-        return redirect('equipamento:listar_equipamentos')
+        form = EquipamentoForm(request.POST, isinstance=equipamento)
+        if form.is_valid():
+            form.save()
+            return redirect('equipamento:listar_equipamentos')
+        else:
+            # Se for GET, mostra o form já preenchido
+            form = EquipamentoForm(isinstance=Equipamento)
+
+        return render(request, 'equipamento/editar_equipamento.html', {'form': form})
     
-    return render(request, 'equipamento/confirmar_exclusao.html', {
-        'objeto': equipamento.descricao,
-        'voltar_url': reverse('equipamento:listar_equipamentos')
-    })
+# EXCLUSÃO DE EQUIPAMENTOS    
+
+class ExcluirEquipamentoView(DeleteView):
+    model = Equipamento 
+    template_name = 'equipamento/excluir_equipamento.html'
+    success_url = reverse_lazy('equipamento:listar_equipamentos')
+
+    def delete(self, request, *args, **kwargs):
+        equipamento = self.get_object()
+
+        # Informações que serão utilizadas na mensagem
+        descricao = equipamento.descricao
+        patrimonio = equipamento.patrimonio
+        # nome = equipamento.nome
+        
+        messages.success(request, 
+                         f'O equipamento {equipamento.nome}, patrimônio número {equipamento.patrimonio}, foi excluído com sucesso.'
+                         )
+        return super().delete(request, *args, **kwargs)
